1f5f3e9e20659ae9e2ffbe326377c37cb8214eb8
Jordi Keijzers <jordikeijzers@gmail.com>
2024-09-30 14:02:46 +0200

    feat: add ninja controller and views

---
+++ NinjaManager/Controllers/NinjaController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NinjaManager.Controllers;
+public class NinjaController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
