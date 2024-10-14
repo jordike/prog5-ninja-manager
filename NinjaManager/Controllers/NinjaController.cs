e94b8d6d8f76605d9e83ef41f9b7c794ff49c98c
Jordi Keijzers <jordikeijzers@gmail.com>
2024-10-14 12:03:31 +0200

    feat: add basic ninja overview

--- NinjaManager/Controllers/NinjaController.cs
+++ NinjaManager/Controllers/NinjaController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NinjaManager.Models;
 
 namespace NinjaManager.Controllers;
 
@@ -6,7 +7,42 @@ public class NinjaController : Controller
 {
     public IActionResult Index()
     {
-        return View();
+        // TODO: Test data, change to actual database content.
+        List<Ninja> ninja =
+        [
+            new Ninja
+            {
+                Id = 1,
+                Name = "Test",
+                Gold = 100
+            },
+            new Ninja
+            {
+                Id = 2,
+                Name = "Test2",
+                Gold = 200
+            },
+            new Ninja
+            {
+                Id = 3,
+                Name = "Test3",
+                Gold = 300
+            },
+            new Ninja
+            {
+                Id = 4,
+                Name = "Test4",
+                Gold = 400
+            },
+            new Ninja
+            {
+                Id = 5,
+                Name = "Test5",
+                Gold = 500
+            }
+        ];
+
+        return View(ninja);
     }
 
     public IActionResult Create()
