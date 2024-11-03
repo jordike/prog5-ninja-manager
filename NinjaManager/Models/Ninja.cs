788331623cbff3730ed6b9d3216aff6cdba3dbe4
Jordi Keijzers <jordikeijzers@gmail.com>
2024-11-03 16:09:51 +0100

    feat: add validation attributes to models

--- NinjaManager/Models/Ninja.cs
+++ NinjaManager/Models/Ninja.cs
@@ -8,8 +8,11 @@ public class Ninja
     public int Id { get; set; }
 
     [Required]
+    [MinLength(1)]
     public string Name { get; set; }
 
+    [Required]
+    [Range(0, int.MaxValue)]
     public int Gold { get; set; }
 
     public List<NinjaHasEquipment> NinjaHasEquipment { get; set; }
