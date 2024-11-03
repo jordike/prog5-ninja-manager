3542aeeb2f0b974a95f84e39b38c8065882622eb
Jordi Keijzers <jordikeijzers@gmail.com>
2024-11-03 22:19:57 +0100

    feat: add documentation to NinjaController

--- NinjaManager/Controllers/NinjaController.cs
+++ NinjaManager/Controllers/NinjaController.cs
@@ -5,15 +5,29 @@ using System.Diagnostics;
 
 namespace NinjaManager.Controllers;
 
+/// <summary>  
+/// Controller for managing ninja-related operations.  
+/// </summary>  
 public class NinjaController : Controller
 {
+    /// <summary>  
+    /// Service for managing ninja-related operations.  
+    /// </summary>  
     private readonly NinjaService _ninjaService;
 
+    /// <summary>  
+    /// Initializes a new instance of the <see cref="NinjaController"/> class.  
+    /// </summary>  
+    /// <param name="context">The context for managing ninja-related data.</param>  
     public NinjaController(NinjaManagerContext context)
     {
         this._ninjaService = new NinjaService(context);
     }
 
+    /// <summary>  
+    /// Displays a list of all ninjas.  
+    /// </summary>  
+    /// <returns>A view displaying the list of ninjas.</returns>  
     public IActionResult Index()
     {
         var ninjas = _ninjaService.GetAllNinjas();
@@ -21,14 +35,23 @@ public class NinjaController : Controller
         return View(ninjas);
     }
 
+    /// <summary>  
+    /// Displays the create ninja form.  
+    /// </summary>  
+    /// <returns>A view displaying the create ninja form.</returns>  
     public IActionResult Create()
     {
-        // Temporary ninja to prevent null reference exception.
+        // Temporary ninja to prevent null reference exception.  
         var ninja = new Ninja();
 
         return View(ninja);
     }
 
+    /// <summary>  
+    /// Handles the creation of a new ninja.  
+    /// </summary>  
+    /// <param name="ninja">The ninja to create.</param>  
+    /// <returns>A redirect to the edit view if successful; otherwise, the create view.</returns>  
     [HttpPost]
     public IActionResult Create(Ninja ninja)
     {
@@ -44,6 +67,11 @@ public class NinjaController : Controller
         return View(ninja);
     }
 
+    /// <summary>  
+    /// Displays the edit ninja form.  
+    /// </summary>  
+    /// <param name="id">The ID of the ninja to edit.</param>  
+    /// <returns>A view displaying the edit ninja form.</returns>  
     public IActionResult Edit(int id)
     {
         var ninja = this._ninjaService.GetNinja(id);
@@ -59,6 +87,11 @@ public class NinjaController : Controller
         return View(ninja);
     }
 
+    /// <summary>  
+    /// Handles the editing of an existing ninja.  
+    /// </summary>  
+    /// <param name="ninja">The ninja to edit.</param>  
+    /// <returns>A redirect to the edit view if successful; otherwise, the edit view.</returns>  
     [HttpPost]
     public IActionResult Edit(Ninja ninja)
     {
@@ -86,6 +119,11 @@ public class NinjaController : Controller
         return RedirectToAction("Edit", ninja);
     }
 
+    /// <summary>  
+    /// Displays the delete ninja confirmation form.  
+    /// </summary>  
+    /// <param name="id">The ID of the ninja to delete.</param>  
+    /// <returns>A view displaying the delete ninja confirmation form.</returns>  
     public IActionResult Delete(int id)
     {
         var ninja = this._ninjaService.GetNinja(id);
@@ -98,6 +136,11 @@ public class NinjaController : Controller
         return View(ninja);
     }
 
+    /// <summary>  
+    /// Handles the deletion of an existing ninja.  
+    /// </summary>  
+    /// <param name="ninja">The ninja to delete.</param>  
+    /// <returns>A redirect to the index view.</returns>  
     [HttpPost]
     public IActionResult Delete(Ninja ninja)
     {
@@ -115,6 +158,11 @@ public class NinjaController : Controller
         return RedirectToAction("Index");
     }
 
+    /// <summary>  
+    /// Displays the clean ninja inventory confirmation form.  
+    /// </summary>  
+    /// <param name="id">The ID of the ninja whose inventory is to be cleaned.</param>  
+    /// <returns>A view displaying the clean ninja inventory confirmation form.</returns>  
     public IActionResult CleanNinja(int id)
     {
         var ninja = this._ninjaService.GetNinja(id);
@@ -127,6 +175,11 @@ public class NinjaController : Controller
         return View(ninja);
     }
 
+    /// <summary>  
+    /// Handles the cleaning of a ninja's inventory.  
+    /// </summary>  
+    /// <param name="ninja">The ninja whose inventory is to be cleaned.</param>  
+    /// <returns>A redirect to the edit view.</returns>  
     [HttpPost]
     public IActionResult CleanNinja(Ninja ninja)
     {
@@ -142,6 +195,10 @@ public class NinjaController : Controller
         return RedirectToAction("Edit", ninja.Id);
     }
 
+    /// <summary>  
+    /// Displays the error view.  
+    /// </summary>  
+    /// <returns>A view displaying the error information.</returns>  
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
